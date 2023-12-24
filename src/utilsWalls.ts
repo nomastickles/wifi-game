f13747b04662a5d57b1ac53df8b7bd87f03c786a
Nomas Tickles <nomastickles@users.noreply.github.com>
2023-12-23 23:14:19 -0800

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! trying stuff out

--- src/utilsWalls.ts
+++ src/utilsWalls.ts
@@ -9,9 +9,6 @@ export function createWalls(game: Engine) {
     name: "wall1",
     pos: vec(width / 2, 0),
     visible: false,
-    width,
-    height: 10,
-    color: Color.Black,
     collisionType: CollisionType.Active,
   });
 
@@ -21,9 +18,6 @@ export function createWalls(game: Engine) {
     name: "wall2",
     pos: vec(width / 2, height),
     visible: false,
-    width,
-    height: 10,
-    color: Color.Black,
     collisionType: CollisionType.Active,
   });
 
@@ -33,9 +27,6 @@ export function createWalls(game: Engine) {
     name: "wall3",
     pos: vec(0, height / 2),
     visible: false,
-    width: 10,
-    height,
-    color: Color.Black,
     collisionType: CollisionType.Active,
   });
 
@@ -45,9 +36,6 @@ export function createWalls(game: Engine) {
     name: "wall4",
     pos: vec(width - 5, height / 2),
     visible: false,
-    width: 10,
-    height,
-    color: Color.Black,
     collisionType: CollisionType.Active,
   });
 
